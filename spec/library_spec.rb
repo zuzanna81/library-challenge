eee6dc060d7092f14050445f9b04680f66058306
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-11-05 14:11:19 +0100

    Added Person class and test to spec

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,6 +2,8 @@ require './lib/library'
 
 describe Library do
 
+  let(:person) { instance_double('Person', name: 'Zuzanna') }
+
   it 'it is expected to display a list of books' do
     expect(subject.books).not_to be_empty
   end
